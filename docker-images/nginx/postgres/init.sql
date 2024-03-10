b30b78ce6da5ef88fa14929ffc4ff73de58c640a
Yossi Ioukhtov <vrectrox@gmail.com>
2024-03-10 22:07:43 +0200

    sticky ips and init schema

---
+++ docker-images/nginx/postgres/init.sql
@@ -0,0 +1,22 @@
+DO $$ BEGIN
+  RAISE NOTICE 'Initializing database schema...';
+END $$;
+
+CREATE DATABASE whist_nginx;
+
+\connect whist_nginx;
+
+CREATE TABLE IF NOT EXISTS global_counter (
+    hit_counter INT
+);
+
+CREATE TABLE IF NOT EXISTS access_log (
+    client_ip inet,
+    date_time timestamp,
+    replica_ip inet
+);
+
+
+DO $$ BEGIN
+  RAISE NOTICE 'Database initialization complete.';
+END $$;
